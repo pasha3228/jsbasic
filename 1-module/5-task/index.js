bae650c96982abd8bfa78b14accdbd59c54af6ee
Dmitry <dmaksimov622@gmail.com>
2022-04-20 22:26:16 +0300

    add truncate(str, maxlength)

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,10 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  let strLength = str.length;
+  if (strLength > maxlength) {
+   str = str.slice(0, maxlength-1) + "…";
+   return str
+  } else {
+    return str;
+  }
 }
+
