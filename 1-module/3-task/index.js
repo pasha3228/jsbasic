75f7c3edabd403e181ab21b29f2bf3227532eeab
Dmitry <dmaksimov622@gmail.com>
2022-04-20 21:48:33 +0300

    add function ucFirst(str)

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,12 @@
 function ucFirst(str) {
-  // ваш код...
+  if ( str != null && str != "") {
+    let firstLetter = str[0];
+    let secondLetters = str.slice(1);
+    firstLetter = firstLetter.toUpperCase();
+    str = firstLetter + secondLetters;
+    return str;
+  } else {
+    return str;
+  }
 }
+
