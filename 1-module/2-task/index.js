8c288e4d3636432af38adef8c1f6fa2309ed6e38
Dmitry <dmaksimov622@gmail.com>
2022-04-20 22:06:09 +0300

    fixed bug in length search

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -12,7 +12,7 @@ function print(text) {
 function isValid(name) {
   if (name != null
     && !(name.includes(" "))
-    && name.lenght >= 4) {
+    && name.length >= 4) {
     return true;
   } else {
     return false;
