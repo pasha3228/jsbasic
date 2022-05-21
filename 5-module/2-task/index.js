1bcc9a96094bd13f00c00768d8c6ec5001002189
Dmitry <dmaksimov622@gmail.com>
2022-05-21 19:40:08 +0300

    add 5 module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+
+  function showHide () {
+    let text = document.querySelector('#text');
+
+    text.hidden == false ? text.hidden = true : text.hidden = false;
+  };
+
+  button.onclick = showHide;
+
+
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,41 @@
 function initCarousel() {
-  // ваш код...
-}
+  const arrowNext = document.querySelector('.carousel__arrow_right');
+  const arrowPrev = document.querySelector('.carousel__arrow_left');
+  const carousel = document.querySelector('.carousel__inner')
+  let currentSlide = 0;
+
+  arrowPrev.style.display = 'none';
+
+  arrowNext.addEventListener('click', moveForward);
+  arrowPrev.addEventListener('click', moveBackward);
+
+
+  function moveForward () {
+    arrowPrev.style.display = ''
+    currentSlide += 1;
+
+    let carouselSlide = document.querySelector('.carousel__slide');
+    let width = carouselSlide.offsetWidth * currentSlide;
+
+    document.querySelector('.carousel__inner').style.transform = `translateX(-${width}px)`;
+
+    if (currentSlide == +carousel.children.length - 1) {
+      arrowNext.style.display = 'none';
+    };
+  };
+
+
+  function moveBackward () {
+    arrowNext.style.display = ''
+    currentSlide -= 1;
+
+    let carouselSlide = document.querySelector('.carousel__slide');
+    let width = carouselSlide.offsetWidth * currentSlide;
+
+    document.querySelector('.carousel__inner').style.transform = `translateX(-${width}px)`;
+
+    if (currentSlide == 0) {
+      arrowPrev.style.display = 'none';
+    };
+  };
+};
